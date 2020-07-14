f59e15b21ff10a0948348e846d2aae9840377043
Oleg Toporkov <oleg.toporkov@coachhub.io>
2020-07-14 16:22:01 +0200

    Added create user test

---
+++ src/api/dto/user/createUserResponseDTO.ts
@@ -0,0 +1,6 @@
+export class CreateUserResponseDTO {
+    public id: string;
+    public name: string;
+    public job: string;
+    public createdAt: string;
+}
---
+++ src/test/api/users.spec.ts
@@ -0,0 +1,28 @@
+import {getRandomInt} from "../../util/random";
+import {ReqresService} from "../../api/service/reqresService";
+import {expect} from "chai";
+import * as moment from "moment";
+
+describe('Reqres service', () => {
+
+    it('should create user', async () => {
+        const userName = `Oleg Toporkov ${getRandomInt()}`;
+        const job = 'QA Automation Engineer';
+
+        const createdBefore = moment();
+
+        const createUserResponseDTO = await ReqresService.createUser(userName, job);
+        console.log(`Created user id is: ${createUserResponseDTO.id}`);
+
+        const createdAfter = moment();
+
+        expect(createUserResponseDTO.id, 'Created user id is incorrect').to.be.a('string').that.is.not.empty;
+        expect(createUserResponseDTO.job, 'Created user job is incorrect').to.be.equal(job);
+        expect(createUserResponseDTO.name, 'Created user name is incorrect').to.be.equal(userName);
+
+        const actualCreatedDate = new Date(createUserResponseDTO.createdAt);
+        expect(actualCreatedDate, 'User creation date is too late').to.be.at.least(createdBefore.toDate());
+        expect(actualCreatedDate, 'User creation date is too early').to.be.at.most(createdAfter.toDate());
+    });
+
+});
